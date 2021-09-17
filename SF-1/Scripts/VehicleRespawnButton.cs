1bb984db62c687fa51634b48ce6208ca5078ae05
esnya <esnya@nekometer.com>
2021-09-17 11:55:11 +0900

    fix: Fix BehaviourSyncMode.None to NoVariableSync

--- SF-1/Scripts/VehicleRespawnButton.cs
+++ SF-1/Scripts/VehicleRespawnButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
-[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+[UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
 public class VehicleRespawnButton : UdonSharpBehaviour
 {
     public EngineController EngineControl;
